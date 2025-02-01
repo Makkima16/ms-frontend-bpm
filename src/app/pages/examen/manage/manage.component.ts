cb35bfa397fbf1b632415c1193b329974a3dd02d
Andres16HKMA <kamzyx2004@gmail.com>
2025-02-01 15:33:31 -0500

    nueva correccion

--- src/app/pages/examen/manage/manage.component.ts
+++ src/app/pages/examen/manage/manage.component.ts
@@ -200,21 +200,18 @@ export class ManageComponent implements OnInit {
           `Respuestas correctas: ${correctAnswersCount}`,
           'success'
         );
-              // Redirigir al usuario
-      this.router.navigate(['courses/list?type='+this.tipo]);
       }else{
         Swal.fire(
           'Fallaste!',
           `Respuestas correctas: ${correctAnswersCount}`,
           'error'
         );
-              // Redirigir al usuario
-      this.router.navigate(['courses/list?type='+this.tipo]);
       }
   
 
   
-
+      // Redirigir al usuario
+      this.router.navigate([`courses/list`], { queryParams: { type: this.tipo}});
   
       // Guardar el registro usando el servicio
       this.record.create(registro).subscribe({
