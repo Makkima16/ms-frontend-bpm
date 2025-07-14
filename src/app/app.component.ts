2f2c128f5250c06d600fc50fe38d6ca1b487fb78
Makkima16 <kamzyx2004@gmail.com>
2025-07-14 10:20:55 -0500

    fix_front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'BPMValencia';
+
+
+  router = inject(Router)
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Esperamos un pequeño tiempo para asegurar que la vista se renderizó
+        setTimeout(() => {
+          const loader = document.getElementById('global-loader');
+          if (loader) {
+            loader.style.display = 'none';
+          }
+        }, 300); // puedes ajustar este tiempo si ves que aún se muestra contenido en blanco
+      }
+    });
+  }
 }
