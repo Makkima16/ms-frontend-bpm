6a690d417cb5872dcf207ffa68e137849a31156e
Makkima16 <kamzyx2004@gmail.com>
2025-10-02 09:02:36 -0500

    fix_logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,10 @@ import { Router, NavigationEnd } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+  export class AppComponent  {
   title = 'BPMValencia';
 
 
   router = inject(Router)
-  ngOnInit(): void {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        // Esperamos un pequeño tiempo para asegurar que la vista se renderizó
-        setTimeout(() => {
-          const loader = document.getElementById('global-loader');
-          if (loader) {
-            loader.style.display = 'none';
-          }
-        }, 300); // puedes ajustar este tiempo si ves que aún se muestra contenido en blanco
-      }
-    });
-  }
+  
 }
--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,24 @@ import { environment } from './environments/environments';
 import LogRocket from 'logrocket';
 
 LogRocket.init('buksaz/bpm-valencia');
+const loader = document.getElementById('global-loader');
+const start = Date.now();
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  .catch(err => console.error(err))
+  .then(() => {
+    const elapsed = Date.now() - start;
+    const remaining = 5000 - elapsed;
+
+    setTimeout(() => {
+      if (loader) {
+        loader.style.opacity = '0';
+        loader.style.transition = 'opacity 0.5s ease';
+        setTimeout(() => loader.remove(), 500);
+      }
+    }, remaining > 0 ? remaining : 0);
+  });
